a28c4b1551c7c29d5473e41525a5540b770a744f
El Acheche Anis <elacheche@ubuntu.com>
2016-01-17 21:52:27 +0100

    Add shebang

--- shell/HUAWEI_E3531.sh
+++ shell/HUAWEI_E3531.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 v=$(lsusb | grep "Huawei" | awk '{ print $6 }' | awk -F: '{ print $1 }'); p=$(lsusb | grep "Huawei" | awk '{ print $6 }' | awk -F: '{ print $2 }'); sudo usb_modeswitch -v $v -p $p -M '55534243123456780000000000000011062000000100000000000000000000'
