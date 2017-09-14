5c8676f80d696525159bdead99a15d461e761261
El Acheche Anis <elacheche@ubuntu.com>
2017-09-14 22:30:14 +0100

    HUAWEI_E3531.sh: Add comments

--- shell/HUAWEI_E3531.sh
+++ shell/HUAWEI_E3531.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-v=$(lsusb | grep "Huawei" | awk '{ print $6 }' | awk -F: '{ print $1 }'); p=$(lsusb | grep "Huawei" | awk '{ print $6 }' | awk -F: '{ print $2 }'); sudo usb_modeswitch -v $v -p $p -M '55534243123456780000000000000011062000000100000000000000000000'
+
+# Get vendor ID
+v=$(lsusb | grep "Huawei" | awk '{ print $6 }' | awk -F: '{ print $1 }')
+
+# Get product ID
+p=$(lsusb | grep "Huawei" | awk '{ print $6 }' | awk -F: '{ print $2 }')
+
+# Enable Modem
+sudo usb_modeswitch -v $v -p $p -M '55534243123456780000000000000011062000000100000000000000000000'
